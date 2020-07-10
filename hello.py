a0211e2dde2388f5de1045db0ef127a4f79dc210
kotalabhay <43821312+kotalabhay@users.noreply.github.com>
2020-07-10 21:42:42 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@
 
 
 
-from cloudant import Cloudant
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import atexit
 import os
@@ -13,37 +12,10 @@ import numpy as np
 
 app = Flask(__name__, static_url_path='',template_folder='static')
 
-db_name = 'mydb'
-client = None
-db = None
 
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif "CLOUDANT_URL" in os.environ:
-    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
-    db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
 
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
 #port=int(os.getenv("VCAP_APP_PORT"))
 
 @app.route('/')
@@ -86,13 +58,7 @@ def emotion():
 # *     "name": "Bob"
 # * }
 # */
-@app.route('/api/visitors', methods=['GET'])
-def get_visitor():
-    if client:
-        return jsonify(list(map(lambda doc: doc['name'], db)))
-    else:
-        print('No database')
-        return jsonify([])
+
 
 # /**
 #  * Endpoint to get a JSON array of all the visitors in the database
@@ -105,22 +71,7 @@ def get_visitor():
 #  * [ "Bob", "Jane" ]
 #  * @return An array of all the visitor names
 #  */
-@app.route('/api/visitors', methods=['POST'])
-def put_visitor():
-    user = request.json['name']
-    data = {'name':user}
-    if client:
-        my_document = db.create_document(data)
-        data['_id'] = my_document['_id']
-        return jsonify(data)
-    else:
-        print('No database')
-        return jsonify(data)
 
-@atexit.register
-def shutdown():
-    if client:
-        client.disconnect()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
